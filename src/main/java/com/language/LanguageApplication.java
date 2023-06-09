dbdc50125f2410f87d64bee36d95cf994836fcf6
YURI HONG <dbfl720@gmail.com>
2023-06-09 19:14:10 +0900

    aws 관련 환경 세팅

--- src/main/java/com/language/LanguageApplication.java
+++ src/main/java/com/language/LanguageApplication.java
@@ -2,11 +2,12 @@ package com.language;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling // batch 어노테이션 
 @SpringBootApplication
-public class LanguageApplication {
+public class LanguageApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LanguageApplication.class, args);
